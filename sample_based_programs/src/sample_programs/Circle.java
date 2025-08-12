3a692e1eb88a51ec47b4e43aced32e620e16d789
Veda1908 <vedasamhita198@gmail.com>
2025-08-12 11:25:03 +0530

    Update Circle.java

--- sample_based_programs/src/sample_programs/Circle.java
+++ sample_based_programs/src/sample_programs/Circle.java
@@ -1,5 +1,16 @@
 package sample_programs;
+import java.util.*;
+public class Circle{
+    public static final double PI = 3.14159;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of the circle: ");
+        double rad = sc.nextDouble();
+        double area = PI * rad * rad;
+        double circ = 2 * PI * rad;
+        System.out.println("circle area is: " + area);
+        System.out.println("circle circumference is: " + circ);
 
-public class Circle {
-
+        sc.close();
+    }
 }
